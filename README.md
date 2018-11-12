5ed601e8f273bd44107160ad3b13e0096da94569
krongkann <krongkan@angstromsolutions.net>
2018-11-12 14:11:25 +0700

    con

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # plugInChrome
+
 How to test
+
+create code and paste to https://mrcoles.com/bookmarklet/
+copy code to bookmark in url and create one bookmark
+when you want to test
+click bookmark
---
+++ concert.js
@@ -0,0 +1,6 @@
+document.addEventListener('keydown', function(event) {
+    if (event.code == "KeyT") {
+        console.log(document.getElementById("nav-toggle").click());
+        document.get
+    }
+});
